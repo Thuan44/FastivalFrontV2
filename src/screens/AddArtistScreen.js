962c0fdc82ecc9c670cf5d1b34dd070c38934ae9
Cong-Thuan Do <congthuan274@gmail.com>
2021-12-05 19:57:46 +0100

    Feature(crud): Add artist feature ok

--- src/screens/AddArtistScreen.js
+++ src/screens/AddArtistScreen.js
@@ -1,49 +1,154 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {Picker} from '@react-native-picker/picker';
 import {
-    View,
-    Text,
-    StyleSheet,
-    Button,
-    ActivityIndicator,
-    TextInput,
-    Alert,
-  } from 'react-native';
-import { artistImages } from '../components/ArtistImages'
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  ActivityIndicator,
+  TextInput,
+  Alert,
+} from 'react-native';
+import {artistImages} from '../components/ArtistImages';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 const stringify = data => {
-    return JSON.parse(JSON.stringify(data))
-}
+  return JSON.parse(JSON.stringify(data));
+};
 
-const AddArtistScreen = ({ route }) => {
-    /* Get the param */
-    // const { item } = route.params;
+const AddArtistScreen = ({route}) => {
+  const [artistName, setartistName] = useState('');
+  const [artistDescription, setartistDescription] = useState('');
+  const [artistImage, setartistImage] = useState('');
+  const [concertDate, setConcertDate] = useState(null);
+  const [concertTime, setConcertTime] = useState(null);
+  const [stageId, setStageId] = useState('');
 
-    return (
-        <View style={styles.container}>
-            <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 20}}>Ajouter un artiste</Text>
-            <SafeAreaView
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: 200,
-            }}>
-            <TextInput
-              style={styles.input}
-              value={""}
-              placeholder="Nom de l'artiste"
-            />
-            <Button
-              title="Ajouter"
-              onPress={() => console.log('Do smthing')}
-              color="salmon"
-              style={styles.button}
-            />
-          </SafeAreaView>
-        </View>
-    )
-}
+  // Redux State Management
+  const {tokenValue} = useSelector(state => state.token);
+  const dispatch = useDispatch();
 
+  const apiUrl = 'http://10.0.2.2:8000/api/artists';
+
+  // Fetch data method
+  const addArtist = (
+    artistName,
+    artistDescription,
+    artistImage,
+    concertDate,
+    concertTime,
+    stageId,
+    token,
+  ) => {
+    // Fetch user data
+    const addAttempt = async () => {
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + token,
+          },
+          body: JSON.stringify({
+            artist_name: artistName,
+            artist_description: artistDescription,
+            artist_img: artistImage,
+            artist_concert_date: concertDate,
+            artist_concert_time: concertTime,
+            stage_id: stageId,
+          }),
+        });
+
+        const json = await response.json();
+        const responseStatus = response.status;
+
+        // Check status code
+        if (responseStatus === 201) {
+          alert('Artist added successfully');
+        } else if (responseStatus >= 400 && responseStatus < 500) {
+          alert('Page not found');
+        } else if (responseStatus >= 500) {
+          alert('Server Error');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    addAttempt();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 20}}>
+        Ajouter un artiste
+      </Text>
+      <SafeAreaView
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: 200,
+        }}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setartistName}
+          value={artistName}
+          placeholder="Nom de l'artiste"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setartistDescription}
+          value={artistDescription}
+          placeholder="Description de l'artiste"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setartistImage}
+          value={artistImage}
+          placeholder="Nom de l'image"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setConcertDate}
+          value={concertDate}
+          placeholder="Date (YYYY-MM-DD)"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setConcertTime}
+          value={concertTime}
+          placeholder="Heure (HH:MM:SS)"
+        />
+        <Picker
+          selectedValue={stageId}
+          style={styles.input}
+          onValueChange={(itemValue, itemIndex) => setStageId(itemValue)}>
+          <Picker.Item value="0" label="Scène" />
+          <Picker.Item label="Scène 1" value="1" />
+          <Picker.Item label="Scène 2" value="2" />
+          <Picker.Item label="Scène 5" value="5" />
+        </Picker>
+        <Button
+          title="Ajouter"
+          onPress={() =>
+            addArtist(
+              artistName,
+              artistDescription,
+              artistImage,
+              concertDate,
+              concertTime,
+              stageId,
+              tokenValue,
+            )
+          }
+          color="salmon"
+          style={styles.button}
+        /> 
+      </SafeAreaView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -65,4 +170,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AddArtistScreen
+export default AddArtistScreen;
